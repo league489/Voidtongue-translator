83e6f1de0a1d1091c59944edc6cc12c79e49f7f8
league489 <rafalkiel33@gmail.com>
2026-06-11 18:32:55 +0200

    Dictionary update 1

--- words.py
+++ words.py
@@ -1,15 +1,15 @@
 import json
 known_words = {
-"Lohk": {"meanings":["void"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
-"Bok": {"meanings":["service"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
-"Grikh": {"meanings":["strength","strong"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun","adjective"],"default_part_of_speech": "ambiguous","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
-"Vak":{"meanings":["desolation","desolate"],"certainty_of_translation":"full","source_of_translation":"in-game translation","part_of_speech_tags": ["noun"],"default_part_of_speech": "ambiguous","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
-"Fass": {"meanings":["chaos"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
-"Ris": {"meanings":["light"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
-"Oth": {"meanings":["gift"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
-"Khra": {"meanings":["time"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
-"Oull": {"meanings":["possibility"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
-"Xata": {"meanings":["truth"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
+"Lohk": {"meanings":["void"],"certainty_of_translation":"full","source_of_translation":"in_game_official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "Lohk", "is_generated": False,"plural_form":"", "notes":""},
+"Bok": {"meanings":["service","servitude","slave(?)"],"certainty_of_translation":"high","source_of_translation":"wiki official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "Bok", "is_generated": False,"plural_form":"", "notes":"'Slave' meaning is not confirmed"},
+"Grikh": {"meanings":["strength","strong"],"certainty_of_translation":"high","source_of_translation":"wiki official","part_of_speech_tags": ["noun","adjective"],"default_part_of_speech": "ambiguous","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "Grikh", "is_generated": False,"plural_form":"", "notes":""},
+"Vak":{"meanings":["desolation","desolate"],"certainty_of_translation":"high","source_of_translation":"wiki official","part_of_speech_tags": ["noun","adjective"],"default_part_of_speech": "ambiguous","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "Vak", "is_generated": False,"plural_form":"", "notes":""},
+"Fass": {"meanings":["chaos"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "Fass", "is_generated": False,"plural_form":"", "notes":""},
+"Ris": {"meanings":["light"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "Ris", "is_generated": False,"plural_form":"", "notes":""},
+"Oth": {"meanings":["gift","give","offer"],"certainty_of_translation":"high","source_of_translation":"wiki official","part_of_speech_tags": ["noun","verb"],"default_part_of_speech": "ambiguous","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "Oth", "is_generated": False,"plural_form":"", "notes":""},
+"Khra": {"meanings":["time"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "Khra", "is_generated": False,"plural_form":"", "notes":""},
+"Oull": {"meanings":["possibility"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "Oull", "is_generated": False,"plural_form":"", "notes":""},
+"Xata": {"meanings":["truth"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "Xata", "is_generated": False,"plural_form":"", "notes":""},
 "Ozun": {"meanings":["dust"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
 "Va": {"meanings":["claw"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["noun"],"default_part_of_speech": "noun","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
 "Yara": {"meanings":["behold"],"certainty_of_translation":"full","source_of_translation":"in-game official","part_of_speech_tags": ["verb"],"default_part_of_speech": "verb","is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_compound": False, "compound_components": [], "compound_rule": "","base_word": "", "is_generated": False,"plural_form":"", "notes":""},
@@ -38,9 +38,24 @@ known_words = {
 #     "-un": {"meanings": ["diminutive (?)"],"certainty_of_translation":"medium","source_of_translation":"wiki_official",  "attaches_to": ["noun", "adjective"], "notes":"Creator agrees with wiki official translation"}
 # },
 # "suffixes_unknown":{
-#    "-tra": {"meanings": ["?"],"certainty_of_translation":"none","source_of_translation":"wiki_official",  "attaches_to": ["noun"], "notes":"Lacks meaning"},
-#     "-li": {"meanings": ["?"],"certainty_of_translation":"none","source_of_translation":"wiki_official",  "attaches_to": ["noun"], "notes":"Lacks meaning"}
+#    "-tra": {"meanings": ["?"],"certainty_of_translation":"none","source_of_translation":"wiki_unknown",  "attaches_to": ["noun"], "notes":"Lacks meaning"},
+#     "-li": {"meanings": ["?"],"certainty_of_translation":"none","source_of_translation":"wiki_unknown",  "attaches_to": ["noun"], "notes":"Lacks meaning"}
 # }
+# }
+
+# prefixes = {
+# "prefixes_productive" :{
+#   "Iv-": {"meanings": ["causative marker"],"certainty_of_translation":"high","source_of_translation":"wiki_official", "attaches_to": ["noun"], "notes":""}, 
+#   "Ka-": {"meanings": ["inversion/'un-(X)'"],"certainty_of_translation":"high","source_of_translation":"wiki_official", "attaches_to": ["noun"], "notes":""},
+#   "Sho-": {"meanings": ["Intensifier / 'Greater'"],"certainty_of_translation":"high","source_of_translation":"wiki_official", "attaches_to": ["noun"], "notes":""},
+# },    
+
+# "prefixes_unknown": {
+#     "V-": {"meanings": ["?"],"certainty_of_translation":"none","source_of_translation":"wiki_unknown",  "attaches_to": ["noun"], "notes":"Lacks meaning"},
+#     "Va-": {"meanings": ["?"],"certainty_of_translation":"none","source_of_translation":"wiki_unknown",  "attaches_to": ["noun"], "notes":"Lacks meaning"},
+#     "Ko-(?)": {"meanings": ["?"],"certainty_of_translation":"none","source_of_translation":"wiki_unknown",  "attaches_to": ["noun"], "notes":"Lacks meaning, not confirmed if it even is a prefix"}
+# }
+        
 # }
 
 
@@ -51,10 +66,10 @@ known_words = {
 # },
 
 # "conjunctions_unknown":{
-#  "Kah": {"meanings": ["?"],"certainty_of_translation":"none","source_of_translation":"wiki_official", "type": "conjunction", "structure": "X Kah Y", "notes":"Lacks meaning"}   
+#  "Kah": {"meanings": ["?"],"certainty_of_translation":"none","source_of_translation":"wiki_unknown", "type": "conjunction", "structure": "X Kah Y", "notes":"Lacks meaning"}   
 # }
 
-# }
+#  }
 
 
 # pronouns = {
@@ -64,6 +79,6 @@ known_words = {
 # }
 # }
 
-# with open("pronouns.json",'w') as pro:
-#     json.dump(pronouns,pro)
-#     pro.close()
+# with open("prefixes.json",'w') as pref:
+#     json.dump(prefixes,pref)
+#     pref.close()
